3e1c228cb3e24d4770ae5a5c1a6f7a57e520bd88
mengxi <iloveloony@gmail.com>
2017-05-10 10:39:27 +0800

    1

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/goods', function(req, res, next) {
+  res.render('category', { title: 'Express' });
+});
+
 module.exports = router;
